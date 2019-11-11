d8e0c9727a4bf67765cd6bf7570064102e170bc3
olov <olov@23be6ad68717>
2019-11-11 17:09:58 +0000

    adding info about where to find MessagingFactory for ServiceLoader

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module se.uu.ub.cora.activemq {
-	// exports se.uu.ub.cora.activemq;
 	requires se.uu.ub.cora.messaging;
 	requires activemq.client;
 	requires javax.jms.api;
 	requires java.naming;
+
+	provides se.uu.ub.cora.messaging.MessagingFactory
+			with se.uu.ub.cora.activemq.ActiveMqMessagingFactory;
 }
